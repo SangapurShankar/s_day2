4537c3ed2fea672a55a22c4bce9d8d534483386b
Good Luck <Roshansangapur@gmail.com>
2022-02-14 00:49:33 +0530

    day2 committed

---
+++ s_day2/src/day2_a2/Demo.java
@@ -0,0 +1,20 @@
+package day2_a2;
+
+class b{
+void get(){
+	System.out.println("Hello this is from outside package");	
+	}
+	
+}
+
+public class Demo  {
+	
+	public static void main(String[] args) {
+		b ob=new b();
+		ob.get();
+		
+		
+		
+	}
+
+}
---
+++ s_day2/src/s_day2_a1/Demo1.java
@@ -0,0 +1,24 @@
+package s_day2_a1;
+
+class b{
+	public void pull(){
+		System.out.println("This is invoked from differnt class");
+	}
+}
+public class Demo1 {
+
+	public static void main(String[] args) {
+		b obj1 = new b();
+		obj1.pull();
+		
+
+	}
+
+	public void pull() {
+		// TODO Auto-generated method stub
+		System.out.println("This is invoked from differnt class Demo1");
+
+		
+	}
+
+}
